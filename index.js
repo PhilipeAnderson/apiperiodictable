dc781099fa40efe3e4accb66b76c29e52d606142
philipe_kraken <philipeacampos@hotmail.com>
2022-01-20 13:28:58 -0300

    Send test api to vercel

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
+let db = [
+  {'1' : {name: 'Hydrogen', atomicNumber: 01, category: 'Nonmetal', atomicMass: 1.080}},
+  {'2' : {name: 'Iron', atomicNumber: 26, category: 'Transition Metal', atomicMass: 55.84}},
+  {'1' : {name: 'Bismuth', atomicNumber: 83, category: 'Post-Transition Metal', atomicMass: 208.98}},
+
+]
+
+app.get('/', (req, res) => {
+  return res.json(db)
+});
+
 app.listen(3333, () => {
   console.log('Started Server')
 });
