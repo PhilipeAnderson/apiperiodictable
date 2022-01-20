9b7777f35dd0a195ca0f0dd7091d06bf6db3b530
philipe_kraken <philipeacampos@hotmail.com>
2022-01-20 16:40:21 -0300

    changed local db for database/database

---
+++ database/database.js
@@ -0,0 +1,6 @@
+const db = [
+  {name: 'Hydrogen', atomicNumber: 01, category: 'Nonmetal', atomicMass: 1.080},
+  {name: 'Iron', atomicNumber: 26, category: 'Transition Metal', atomicMass: 55.84},
+  {name: 'Bismuth', atomicNumber: 83, category: 'Post-Transition Metal', atomicMass: 208.98},
+  {name: 'Niobium', atomicNumber: 41, category: 'Transition Metal', atomicMass: 92.90},
+]
