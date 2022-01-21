4088e8eda149dc636fb0b9e7ec31e385483772ba
philipe_kraken <philipeacampos@hotmail.com>
2022-01-21 12:27:57 -0300

    Removed /clientapiperiodictable and Added /frontend

--- clientapiperiodictable/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-
-import '../styles/global.scss';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
