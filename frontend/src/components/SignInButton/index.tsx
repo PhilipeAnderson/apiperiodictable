0173a24e352f6343e9165e778b744a5b8ea6493b
philipe_kraken <philipeacampos@hotmail.com>
2022-04-14 13:12:34 -0300

    Change status of the login with useState

--- frontend/src/components/SignInButton/index.tsx
+++ frontend/src/components/SignInButton/index.tsx
@@ -1,21 +1,43 @@
+import { useState, useEffect } from 'react';
+
 import { FaGithub} from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
-const isUserLoggedIn = false;
+//const isUserLoggedIn = false;
 
 export function SignInButton() {
+
+  const [ isUserLoggedIn, setIsUserLoggedIn ] = useState(false)
+
+  const changeLogged = () => {
+    setIsUserLoggedIn(true)
+  }
+
+  useEffect(() => {
+    
+  })
+
+
   return isUserLoggedIn ? (
-    <button type="button" className={ styles.signInButton }>
+    <button 
+      type="button" 
+      className={ styles.signInButton }
+      
+    >
       <FaGithub color="#04d361"/>
       Philipe Anderson
-      <FiX color="#737380" className={ styles.closeIcon } />
+      <FiX color="#737380" className={ styles.closeIcon } onClick={() => setIsUserLoggedIn(false)}/>
     </button>   
   ) : (
-    <button type="button" className={ styles.signInButton }>
+    <button 
+      type="button"
+      onClick={() => changeLogged()}
+      className={ styles.signInButton }
+    >
       <FaGithub color="#fede00"/>
-      Sign in
+        Sign in
     </button>
   )
 }
