3fb3e6c76530891e089b1ee3f6b7d7f51e46f835
philipe_kraken <philipeacampos@hotmail.com>
2022-04-20 11:02:47 -0300

    Fix problem for receiving data login from Github

--- frontend/src/components/SignInButton/index.tsx
+++ frontend/src/components/SignInButton/index.tsx
@@ -6,9 +6,9 @@ import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-  const session = useSession()
+  const {data: session} = useSession()
 
-
+  console.log(session)
 
   return session ? (
     <button 
@@ -17,6 +17,7 @@ export function SignInButton() {
     onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
+      {session.user.name}
       <FiX 
         color="#737380" 
         className={ styles.closeIcon }
