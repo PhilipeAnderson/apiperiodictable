2d20f804978969f6988ac96757a9f0cc3bb397b9
brunowilian <brunowiliancrispim@gmail.com>
2022-04-20 16:35:54 -0300

    refactored howuse

--- frontend/src/pages/howuse.tsx
+++ frontend/src/pages/howuse.tsx
@@ -5,9 +5,9 @@ export default function Howuse() {
 
   return (
     <main className={styles.container}>
+      <div className={styles.usageExamples}>
       <h1>How Use</h1>
       <h2>Super Easy</h2>
-      <div className={styles.usageExamples}>
         <p>
           Just use tradicional methods of query a JSON document, you can set with axios or fetchAPI for javascript for exemple.
           The url for query api is: <a href='https://apiperiodictable.herokuapp.com/' target="_blank" rel="noopener noreferrer"> Link API</a>  its a JSON format!
