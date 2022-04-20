fddd3e4c4483f0d90d4e573a75c1fdb0260b9d53
philipe_kraken <philipeacampos@hotmail.com>
2022-04-20 12:55:51 -0300

    Created the footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
+import { BuyMeACoffee } from '../BuyMeACoffee';
 import styles from './styles.module.scss';
 
 export function Footer() {
   return(
     <main className={styles.container}>
-      Footer
+      <div className={styles.content}>
+        <Image
+          src="/img/hero.gif"
+          alt="Logo API"
+          width={200}
+          height={200}
+        />
+        <section>
+          <h3>Help The Project</h3>
+          <BuyMeACoffee />
+        </section>
+      </div>
     </main>
   )
 }
