0e14335ddd953ad6fca8ad221868ecc3f0c15536
philipe_kraken <philipeacampos@hotmail.com>
2022-04-20 14:46:49 -0300

    Replaced the main tag with the Footer tag in layout the component Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,12 +2,12 @@ import styles from './styles.module.scss';
 
 export function Footer() {
   return(
-    <main className={styles.container}>
+    <footer className={styles.container}>
       <div className={styles.content}>
         <h3>logo</h3>
         <h3>helpUs</h3>
         <h3>Contact US</h3>
       </div>
-    </main>
+    </footer>
   )
 }
