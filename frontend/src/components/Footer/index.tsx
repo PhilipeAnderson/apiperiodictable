559201ae71c448fcc5b8ae0f9906249beab9d993
philipe_kraken <philipeacampos@hotmail.com>
2022-04-20 13:00:17 -0300

    New info in footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,21 +1,12 @@
-import Image from 'next/image';
-import { BuyMeACoffee } from '../BuyMeACoffee';
 import styles from './styles.module.scss';
 
 export function Footer() {
   return(
     <main className={styles.container}>
       <div className={styles.content}>
-        <Image
-          src="/img/hero.gif"
-          alt="Logo API"
-          width={200}
-          height={200}
-        />
-        <section>
-          <h3>Help The Project</h3>
-          <BuyMeACoffee />
-        </section>
+        <h3>logo</h3>
+        <h3>helpUs</h3>
+        <h3>Contact US</h3>
       </div>
     </main>
   )
