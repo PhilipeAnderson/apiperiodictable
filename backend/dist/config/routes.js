5fc155f8eff5ae4109ceeb4eed90239464067157
philipe_kraken <philipeacampos@hotmail.com>
2022-04-25 18:55:41 -0300

    Created folder ./dist for receiving data from teypescript compile

---
+++ backend/dist/config/routes.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require('express');
+const routes = express();
+const db = require('../database/database');
+routes.get('/', (req, res) => {
+    return res.json(db);
+});
+module.exports = routes;
